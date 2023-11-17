c7fb6fe5d887fa3a080422a3fd454faf0ca73622
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2023-11-17 09:14:58 +0700

    membuat navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,88 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset("/assets/css/global.css") }}">
+    <link rel="stylesheet" href="{{ asset("/assets/css/welcome.css") }}">
 </head>
 <body>
-    
+{{--    navbar start --}}
+    <nav class="navbar bg-body-tertiary navbar-expand-xl">
+        <div class="container-fluid">
+{{--            tampilan hp--}}
+            <a class="d-flex d-sm-none align-items-center navbar-brand" href="/">
+                <img src="{{ asset("/assets/img/Logo Institut Teknologi Del.png") }}" alt="Logo" width="35" height="35" class="d-inline-block align-text-top">
+                <div class="ms-3 lh-1">
+                    <span class="fs-3"><strong>SPMB IT Del</strong></span>
+                </div>
+            </a>
+{{--            tampilan hp end--}}
+
+{{--            tampilan desktop--}}
+            <a class="d-none d-sm-flex align-items-center navbar-brand" href="/">
+                <img src="{{ asset("/assets/img/Logo Institut Teknologi Del.png") }}" alt="Logo" width="45" height="45" class="d-inline-block align-text-top">
+                <div class="ms-3 lh-1">
+                    <span><strong>SPMB Institut Teknologi Del</strong></span>
+                    <br>
+                    <span class="fs-7"><i>MarTuhan, MarRoha, MarBisuk</i></span>
+                </div>
+            </a>
+{{--            tampilan desktop end--}}
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto me-3">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Program
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" aria-current="page" href="#">Admisi</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="dropdown-item">Test</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kontak</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Prestasi</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Beasiswa</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Akreditasi</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FAQ</a>
+                    </li>
+                </ul>
+                <a class="d-none d-xl-inline">
+                    <button class="btn btn-primary">Pengumuman SPMB</button>
+                </a>
+                <a class="d-xl-none text-primary text-decoration-none">
+                    <span><strong>Pengumuman SPMB</strong></span>
+                </a>
+            </div>
+        </div>
+    </nav>
+{{--    navbar end--}}
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
